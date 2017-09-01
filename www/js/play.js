0daca83a030761d05c880fb5c493838c11cadeac
lielvan <lielvan1234@gmail.com>
2017-09-01 13:44:39 +0300

    Player-turn works with a loser decided at the end

--- www/js/play.js
+++ www/js/play.js
@@ -1,6 +1,26 @@
 var names = JSON.parse(window.sessionStorage.getItem("names"));
 function play() {
+    document.getElementById("test").innerHTML = "";
     var numberOfTurns = Math.floor(Math.random() * (25-15) + 15); //Randomly picks a number between 15-25
-
+    var currentTurn = 1;
+    var iter = 0;
     document.getElementById("demo").innerHTML = names;
+
+    while(currentTurn <= numberOfTurns) {
+      if(iter<=names.length-1) {
+        var para = document.createElement("p");                                                      // Create a <p> node
+        var t = document.createTextNode("Current Turn: "+currentTurn+names[iter]);                   // Create a text node
+        para.appendChild(t);                                                                        // Append the text to <p>
+        document.getElementById("test").appendChild(para);                                          // Append <p> to <div> with id="myDIV"
+        // Insert "Next Turn" button features below
+
+
+        currentTurn++;
+        iter++;
+      }
+      else {
+        iter = 0;
+      }
+    }
+    document.getElementById("endgame").innerHTML = names[iter-1] + " YOU LOST!";
 }
